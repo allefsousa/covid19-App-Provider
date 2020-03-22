95652bbea93251b8c0f0e468dd949ccf148ab1c8
Allef Sousa <allefsousa_1@hotmail.com>
2020-03-21 23:37:14 -0300

    Criando a classe Home Provider

---
+++ lib/providers/home_provider.dart
@@ -0,0 +1,20 @@
+import 'package:covid19/models/home_model.dart';
+import 'package:covid19/services/api_services.dart';
+import 'package:flutter/material.dart';
+
+class HomeProvider with ChangeNotifier{
+  var api = ApiServices();
+  HomeModel home;
+  getHomeProvider() async{
+    final response = await api.client.get("${api.baseUrl}/api");
+    if(response.statusCode == 200){
+      notifyListeners();
+          var res = homeModelFromJson(response.body);
+      home = res;
+      return res;
+    }else{
+      return null;
+    }
+  }
+
+}
